bf417fea424db2fe577c74d66c688b10d21e8940
George Dong <georgedong0501@gmail.com>
2024-04-22 10:19:28 -0400

    Create initial workspace and initialized Amplify in application

---
+++ app/src/main/java/com/example/aguadatosapp/AguaDatosAmplifyApp.kt
@@ -0,0 +1,22 @@
+package com.example.aguadatosapp
+
+import android.app.Application
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
+//import com.amplifyframework.datastore.AWSDataStorePlugin
+
+
+class AguaDatosAmplifyApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        try {
+            //Amplify.addPlugin(AWSDataStorePlugin())
+            Amplify.configure(applicationContext)
+            Log.i("AguaDatosAmplifyApp", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("AguaDatosAmplifyApp", "Could not initialize Amplify", error)
+        }
+    }
+}
