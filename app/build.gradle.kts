e840870c6097cfaa88205821140f312934755764
beccaj95 <becca.jeffries95@gmail.com>
2024-07-09 14:36:24 -0400

    added resource files for raw water and plant flow view submission pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-<<<<<<< HEAD
     implementation("androidx.compose.ui:ui-text-android:1.6.8")
-=======
->>>>>>> main
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
