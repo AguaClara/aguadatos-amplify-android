bf8679877bdf8cb116fa101e1e6471b34eb9926b
jlfud <69946005+jlfud@users.noreply.github.com>
2024-09-11 17:23:25 -0400

    changed graph functionality to buttons

--- app/src/main/java/com/example/aguadatosapp/GraphsFragment.kt
+++ app/src/main/java/com/example/aguadatosapp/GraphsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
@@ -12,40 +13,39 @@ import com.github.mikephil.charting.data.LineDataSet
 
 class GraphsFragment : Fragment() {
 
+    private lateinit var graph: LineChart
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_graphs, container, false)
-        val graph1: LineChart = view.findViewById(R.id.graph1)
-        val graph2: LineChart = view.findViewById(R.id.graph2)
-        val graph3: LineChart = view.findViewById(R.id.graph3)
-        val graph4: LineChart = view.findViewById(R.id.graph4)
-        val graph5: LineChart = view.findViewById(R.id.graph5)
-        val graph6: LineChart = view.findViewById(R.id.graph6)
-        val graph7: LineChart = view.findViewById(R.id.graph7)
-        val graph8: LineChart = view.findViewById(R.id.graph8)
 
-        //set up graphs
-        setUpGraph(graph1)
-        setUpGraph(graph2)
-        setUpGraph(graph3)
-        setUpGraph(graph4)
-        setUpGraph(graph5)
-        setUpGraph(graph6)
-        setUpGraph(graph7)
-        setUpGraph(graph8)
+        graph = view.findViewById(R.id.graph)
+
+        val btnGraph1: Button = view.findViewById(R.id.btnPF)
+        val btnGraph2: Button = view.findViewById(R.id.btnRWT)
+        val btnGraph3: Button = view.findViewById(R.id.btnCoD)
+        val btnGraph4: Button = view.findViewById(R.id.btnFWT)
+        val btnGraph5: Button = view.findViewById(R.id.btnCWT)
+        val btnGraph6: Button = view.findViewById(R.id.btnChD)
+
+        btnGraph1.setOnClickListener { setUpGraph("Plant Flow") }
+        btnGraph2.setOnClickListener { setUpGraph("Raw Water Turbidity") }
+        btnGraph3.setOnClickListener { setUpGraph("Coagulant Dosage") }
+        btnGraph4.setOnClickListener { setUpGraph("Filtered Water Turbidity") }
+        btnGraph5.setOnClickListener { setUpGraph("Clarified Water Turbidity") }
+        btnGraph6.setOnClickListener { setUpGraph("Chlorine Dosage") }
+
+        setUpGraph("Plant Flow")
 
         return view
     }
 
-    private fun setUpGraph(graph: LineChart) {
-        val entries = ArrayList<Entry>()
-        //random data points
-        for (i in 0..10) {
-            entries.add(Entry(i.toFloat(), (Math.random() * 100).toFloat()))
-        }
-        val dataSet = LineDataSet(entries, "Graph Data")
+    // Function to set up graph with data
+    private fun setUpGraph(graphName: String) {
+        val data = arrayListOf(Entry(1f, 1f), Entry(2f, 2f))
+        val dataSet = LineDataSet(data, "$graphName Data")
         val lineData = LineData(dataSet)
         graph.data = lineData
         graph.invalidate()
