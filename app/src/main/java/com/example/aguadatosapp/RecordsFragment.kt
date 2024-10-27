57509b1c92f4ef99a4e6d6a3bdbf20f10efa2b6f
Your Name <Patrickegan256@gmail.com>
2024-10-27 15:23:08 -0400

    Updated records page
    - Seperated each entry type into its own XML file
    - Made each data field editable (for some entries)

--- app/src/main/java/com/example/aguadatosapp/RecordsFragment.kt
+++ app/src/main/java/com/example/aguadatosapp/RecordsFragment.kt
@@ -130,71 +130,77 @@ class RecordsFragment : Fragment() {
 
     private fun showEditEntryDialog(entry: Entry) {
         // Inflate the custom layout for the dialog
-        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.record_edit_entry, null)
-
-        // Initialize views inside the dialog
+        val dialogView : View = when (entry) {
+            is PlantFlowEntry -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_calibration_entry, null)
+            }
+            is TurbidityEntry -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_calibration_entry, null)
+            }
+            is FilteredWaterEntry -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_calibration_entry, null)
+            }
+            is CalibrationEntry -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_calibration_entry, null)
+            }
+            is ChangeDoseEntry -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_calibration_entry, null)
+            }
+            is FeedbackEntry -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_feedback_entry, null)
+            }
+            else -> {
+                LayoutInflater.from(requireContext()).inflate(R.layout.edit_calibration_entry, null)
+            }
+        }
         val closeButton = dialogView.findViewById<Button>(R.id.close_button)
         val deletedButton = dialogView.findViewById<Button>(R.id.delete_button)
         val saveButton = dialogView.findViewById<Button>(R.id.save_button)
-        val titleTextView = dialogView.findViewById<TextView>(R.id.title)
-        val timeTextView = dialogView.findViewById<TextView>(R.id.time_text)
-        // ChangeDoseEntry
-        val sliderPosText = dialogView.findViewById<TextView>(R.id.slider_position_text)
-        val inflowRateText = dialogView.findViewById<TextView>(R.id.inflow_rate_text)
-        val startHeightText = dialogView.findViewById<TextView>(R.id.start_height_text)
-        val endHeightText = dialogView.findViewById<TextView>(R.id.end_height_text)
-        val timeElapsedText = dialogView.findViewById<TextView>(R.id.time_elapsed_text)
-        val chemicalDoseText = dialogView.findViewById<TextView>(R.id.chemical_dose_text)
-        val chemicalFlowRateText = dialogView.findViewById<TextView>(R.id.chemical_flow_rate_text)
-        val chemicalTypeTextView = dialogView.findViewById<TextView>(R.id.chemical_type_text)
-        val calibrationTextView = dialogView.findViewById<TextView>(R.id.calibration_text)
-        // FeedbackEntry
-        val feedbackText = dialogView.findViewById<TextView>(R.id.feedback_text)
-
+        val titleText = dialogView.findViewById<TextView>(R.id.title)
+        val timeText = dialogView.findViewById<TextView>(R.id.time_text)
 
         when (entry) {
-            is CalibrationEntry -> {
-                titleTextView.text = entry.name
-                timeTextView.text = (entry.date + "   " + entry.time)
-                chemicalTypeTextView.text = "Chemical Type: PAC "
-                sliderPosText.text = ("• Slider Position: ${entry.sliderPosition} %")
-                inflowRateText.text = ("• Inflow Rate: ${entry.inflowRate} mL/s")
-                startHeightText.text = ("• Start Height: ${entry.startVolume} mL")
-                endHeightText.text = ("• End Height: ${entry.endVolume} mL")
-                timeElapsedText.text = ("• Time Elapsed: ${entry.timeElapsed} s")
-                chemicalDoseText.text = ("• Chemical Dose: ${entry.chemDose} mg/L")
-                chemicalFlowRateText.text = ("• Chemical Flow Rate: ${entry.chemFlowRate} mL/s")
-                feedbackText.visibility = View.GONE
+            is PlantFlowEntry -> {
+                titleText.text = entry.name
+                timeText.text = (entry.date + "   " + entry.time)
             }
             is TurbidityEntry -> {
-                titleTextView.text = entry.name
-                timeTextView.text = (entry.date + "\t" + entry.time)
-                chemicalTypeTextView.visibility = View.GONE
-                calibrationTextView.visibility = View.GONE
-                chemicalTypeTextView.visibility = View.GONE
-                sliderPosText.visibility = View.GONE
-                inflowRateText.visibility = View.GONE
-                startHeightText.visibility = View.GONE
-                endHeightText.visibility = View.GONE
-                timeElapsedText.visibility = View.GONE
-                chemicalDoseText.visibility = View.GONE
-                chemicalFlowRateText.visibility = View.GONE
-                feedbackText.visibility = View.GONE
+                titleText.text = entry.name
+                timeText.text = (entry.date + "   " + entry.time)
+            }
+            is FilteredWaterEntry -> {
+                titleText.text = entry.name
+                timeText.text = (entry.date + "   " + entry.time)
+            }
+            is CalibrationEntry -> {
+                titleText.text = entry.name
+                timeText.text = (entry.date + "   " + entry.time)
+                val sliderPos = dialogView.findViewById<EditText>(R.id.edit_slider_position)
+                val inflowRate = dialogView.findViewById<EditText>(R.id.edit_inflow_rate)
+                val startHeight = dialogView.findViewById<EditText>(R.id.edit_start_height)
+                val endHeight = dialogView.findViewById<EditText>(R.id.edit_end_height)
+                val timeElapsed = dialogView.findViewById<EditText>(R.id.edit_time_elapsed)
+                val chemicalDose = dialogView.findViewById<EditText>(R.id.edit_chemical_dose)
+                val chemicalFlowRate = dialogView.findViewById<EditText>(R.id.edit_chemical_flow_rate)
+                val chemicalTypeText = dialogView.findViewById<TextView>(R.id.chemical_type_text)
+                chemicalTypeText.setText("Chemical Type: PAC ")
+                sliderPos.setText("${entry.sliderPosition}")
+                inflowRate.setText("${entry.inflowRate}")
+                startHeight.setText("${entry.startVolume}")
+                endHeight.setText("${entry.endVolume}")
+                timeElapsed.setText("${entry.timeElapsed}")
+                chemicalDose.setText("${entry.chemDose}")
+                chemicalFlowRate.setText("${entry.chemFlowRate}")
+            }
+            is ChangeDoseEntry -> {
+                titleText.text = entry.name
+                timeText.text = (entry.date + "   " + entry.time)
             }
             is FeedbackEntry -> {
-                titleTextView.text = entry.name
-                timeTextView.text = (entry.date + "\t" + entry.time)
-                feedbackText.text = "Feedback: ${entry.feedback}"
-                chemicalTypeTextView.visibility = View.GONE
-                calibrationTextView.visibility = View.GONE
-                chemicalTypeTextView.visibility = View.GONE
-                sliderPosText.visibility = View.GONE
-                inflowRateText.visibility = View.GONE
-                startHeightText.visibility = View.GONE
-                endHeightText.visibility = View.GONE
-                timeElapsedText.visibility = View.GONE
-                chemicalDoseText.visibility = View.GONE
-                chemicalFlowRateText.visibility = View.GONE
+                titleText.text = entry.name
+                timeText.text = (entry.date + "   " + entry.time)
+                val feedbackText = dialogView.findViewById<EditText>(R.id.edit_feedback)
+
             }
         }
 
