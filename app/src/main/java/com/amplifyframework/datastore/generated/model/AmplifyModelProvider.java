3a8aec4e980c10ccaeb0cc9bddf4ddd7fe69af22
George Dong <67238757+GeorgeDong00@users.noreply.github.com>
2024-10-27 15:32:57 -0400

    Merge amplify-schema-init into main – Created GraphQL API with Entry Types (#14)
    This PR introduces significant updates to the GraphQL schema and deploys
    the enhanced API to the main branch. The primary changes include:
    
    GraphQL Schema Improvements:
    
    New Enums:
    EntryType: Defines various types of `entries` such as `PLANT_FLOW`,
    `RAW_TURBIDITY`, `CLARIFIED_TURBIDITY`, etc.
    ChemType: Specifies the types of chemicals used, including
    `POLY_ALUMINIUM_CHLORIDE` and `ALUMINUM_SULFATE`.
    Updated Entry Model:
    Primary Key Configuration: Partition by `plantName` and sort by
    `entryType` and `creationDate`.
    Global Secondary Index: Added `operatorName` with a sort key on
    `creationDate` to enable querying entries by operator.
    Contingent Fields: Fields specific to different `EntryType` variants,
    such as calibration and dosage details.
    Feedback Field: Added `operatorFeedback` for capturing operator inputs.
    
    Amplify Configuration Updates:
    Code Generation: Enabled automatic generation of all possible GraphQL
    operations (queries, mutations, subscriptions) with a maximum statement
    depth of 2.
    Deployment: Deployed resources to the dev environment, including updates
    to AWS AppSync, IAM roles, DynamoDB tables, and CloudFormation stacks.

--- app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "aeb782e737a8d70b304eaac102552d89";
+  private static final String AMPLIFY_MODEL_VERSION = "6a60d1cfa3ecb16960c1d3140144368e";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
@@ -34,7 +34,7 @@ public final class AmplifyModelProvider implements ModelProvider {
   @Override
    public Set<Class<? extends Model>> models() {
     final Set<Class<? extends Model>> modifiableSet = new HashSet<>(
-          Arrays.<Class<? extends Model>>asList()
+          Arrays.<Class<? extends Model>>asList(Entry.class)
         );
     
         return Immutable.of(modifiableSet);
