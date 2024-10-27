afeadfa8bb48f7e63610ecc1d459d966a541de26
George Dong <georgedong51@gmail.com>
2024-10-27 15:20:13 -0400

    Initialized Amplify instance with Entry schemas

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "aguadatosamplifyapp": {
+    "aguadatosamplify": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
-    }
-  },
-  "auth": {
-    "aguadatosamplifyapp05edfa89": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
     }
   }
 }
